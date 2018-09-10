c7b9525be6b843a52801c1ca00fa181602cb5790
Vadim <43129458+Vadim797@users.noreply.github.com>
2018-09-10 10:53:34 +0300

    Add files via upload
    HTML muudetud PHP-ks

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset='utf-8'>
+    <title>Vadim Žigalin, õppetöö</title>
+  </head>
+ 
+  <body>
+  
+    <h1>Vadim Žigalin</h1>
+  
+    <p>Siin on minu <a href="http://www.tlu.ee">TLÜ</a> õppetöö raames valminud veebilehed.</p>
+      <!-- <p><img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt=<"Terra hoone"/p> >-->
+    <p><img src="tlu_terra_600x400_1" alt=<"TLÜ Terra hoone"/p>
+	<p>Mul on sõber, kes ka teeb <a href="http://greeny.cs.tlu.ee/~jevgpro/index.html">veebi</a>.
+    <p><a href="Kodutöö%231.html">1. Esimene kodutöö</a></p>
+	
+  </body>
+  
+</html>
